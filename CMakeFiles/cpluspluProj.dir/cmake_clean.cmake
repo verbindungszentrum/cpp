b0a62f9cda29028f981365c30c29a205ee9ac2c9
C Plus Plus <cplusplus@verbindungszentrum.com>
2021-02-22 22:19:46 +0100

    add missing CMake configuration files

---
+++ CMakeFiles/cpluspluProj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpluspluProj.dir/main.cpp.o"
+  "cpluspluProj"
+  "cpluspluProj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpluspluProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/skiti/Desktop/cpp
+# Build directory: /home/skiti/Desktop/cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
